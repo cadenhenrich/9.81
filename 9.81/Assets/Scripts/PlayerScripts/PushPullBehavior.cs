8bf8ad25dd09f8ad92f1f3e4c29ddb06380ebfb5
Caden Henrich <cadenhenrich@gmail.com>
2023-04-14 22:00:44 -0400

    Added radii for pushing/pulling

--- 9.81/Assets/Scripts/PlayerScripts/PushPullBehavior.cs
+++ 9.81/Assets/Scripts/PlayerScripts/PushPullBehavior.cs
@@ -4,82 +4,93 @@ using UnityEngine;
 
 public class PushPullBehavior : MonoBehaviour
 {
-  [Header("Push")]
-  [SerializeField]
-  private float pushForce;
-  [SerializeField]
-  private float pushRadius;
-  [SerializeField]
-  private float pushCooldown;
+    [Header("Push")]
+    [SerializeField]
+    private float pushForce;
+    [SerializeField, Tooltip("The radius in which objects will be pushed")]
+    private float pushEffectRadius;
+    [SerializeField, Tooltip("The radius around the player where the effect can be used (set to 0 for infinite radius)")]
+    private float pushUseRadius;
+    [SerializeField]
+    private float pushCooldown;
 
-  private bool hasPushed;
+    private bool hasPushed;
 
-  [Header("Pull")]
-  [SerializeField]
-  private float pullForce;
-  [SerializeField]
-  private float pullRadius;
-  [SerializeField]
-  private float pullCooldown;
+    [Header("Pull")]
+    [SerializeField]
+    private float pullForce;
+    [SerializeField, Tooltip("The radius in which objects will be pulled")]
+    private float pullEffectRadius;
+    [SerializeField, Tooltip("The radius around the player where the effect can be used (set to 0 for infinite radius)")]
+    private float pullUseRadius;
+    [SerializeField]
+    private float pullCooldown;
 
-  private bool hasPulled;
+    private bool hasPulled;
+    private Transform playerTransform;
 
-  void Start()
-  {
-    
-  }
-
-  void Update()
-  {
-    if (Input.GetAxisRaw("Fire1") > 0 && !hasPushed)
+    void Start()
     {
-      Push(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
-    else if (Input.GetAxisRaw("Fire2") > 0 && !hasPulled)
+
+    void Update()
     {
-      Pull(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        if (Input.GetAxisRaw("Fire1") > 0 && !hasPushed)
+        {
+            Push(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+        else if (Input.GetAxisRaw("Fire2") > 0 && !hasPulled)
+        {
+            Pull(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
     }
-  }
 
-  void Push(Vector2 coordinates)
-  {
-    hasPushed = true;
-    ApplyGravityEffect(coordinates, pushForce);
-    StartCoroutine(PushCooldown());
-  }
-
-  void Pull(Vector2 coordinates)
-  {
-    hasPulled = true;
-    ApplyGravityEffect(coordinates, -pullForce);
-    StartCoroutine(PullCooldown());
-  }
-
-  void ApplyGravityEffect(Vector2 coordinates, float force)
-  {
-    Collider2D[] overlaps = Physics2D.OverlapCircleAll(coordinates, pushRadius);
-    foreach (Collider2D collider in overlaps)
+    void Push(Vector2 coordinates)
     {
-      Rigidbody2D rigidbody = collider.GetComponent<Rigidbody2D>();
-      if (rigidbody != null)
-      {
-        Vector2 delta = rigidbody.position - coordinates;
-        rigidbody.AddForce(delta.normalized * force + Vector2.up * force / 2f, ForceMode2D.Impulse);
-      }
+        if (pushUseRadius != 0 && Vector2.Distance(playerTransform.position, coordinates) <= pushUseRadius)
+        {
+            hasPushed = true;
+            ApplyGravityEffect(coordinates, pushForce, pushUseRadius);
+            StartCoroutine(PushCooldown());
+        }
     }
-  }
 
-  IEnumerator PushCooldown()
-  {
-    yield return new WaitForSeconds(pushCooldown);
+    void Pull(Vector2 coordinates)
+    {
+        if (pullUseRadius != 0 && Vector2.Distance(playerTransform.position, coordinates) <= pullUseRadius)
+        {
+            hasPulled = true;
+            ApplyGravityEffect(coordinates, -pullForce, pullEffectRadius);
+            StartCoroutine(PullCooldown());
+        }
+    }
 
-    hasPushed = false;
-  }
+    void ApplyGravityEffect(Vector2 coordinates, float force, float radius)
+    {
+        Collider2D[] overlaps = Physics2D.OverlapCircleAll(coordinates, radius);
+        foreach (Collider2D collider in overlaps)
+        {
+            Rigidbody2D rigidbody = collider.GetComponent<Rigidbody2D>();
+            if (rigidbody != null && !rigidbody.CompareTag("Player"))
+            {
+                Vector2 delta = rigidbody.position - coordinates;
+                rigidbody.AddForce(delta.normalized * force + Vector2.up * force / 2f, ForceMode2D.Impulse);
+            }
+        }
+    }
 
-  IEnumerator PullCooldown()
-  {
-    yield return new WaitForSeconds(pullCooldown);
+    IEnumerator PushCooldown()
+    {
+        yield return new WaitForSeconds(pushCooldown);
 
-    hasPulled = false;
-  }
+        hasPushed = false;
+    }
+
+    IEnumerator PullCooldown()
+    {
+        yield return new WaitForSeconds(pullCooldown);
+
+        hasPulled = false;
+    }
 }
